b1218a775ab44ffdd2adc03adc6a2b3eb3d7c6d7
SirVoRaK <sirvoraky@gmail.com>
2021-07-06 21:41:11 -0300

    scroll to top button added

--- script.js
+++ script.js
@@ -1,16 +1,23 @@
 const delay = 5000
 const container = document.querySelector("div.container")
+document.querySelector("a.top").style.display = "none"
 setTimeout(function(){
     container.style.display = "flex"
 }, delay)
 
 document.onscroll = (e) => {
     let y = window.pageYOffset
+    /* console.log(y) */
     if(y > 200){
         document.querySelector("div.content.chess div.inner").style.animation = "floatfromright 2s ease forwards"
     }if(y > 1190){
         document.querySelector("div.content.snake div.inner").style.animation = "floatfromright 2s ease forwards"
     }if(y > 2250){
         document.querySelector("div.content.tictactoe div.inner").style.animation = "floatfromright 2s ease forwards"
+    }
+    if(y < 70){
+        document.querySelector("a.top").style.display = "none"
+    }else{
+        document.querySelector("a.top").style = ""
     }
 }
